c89d9c10f2108e375162c63c8adc58bc444df13d
Shah Rukh Khan <shah@crowdbotics.com>
2020-09-09 15:37:52 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,25 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
+    name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +33,8 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
